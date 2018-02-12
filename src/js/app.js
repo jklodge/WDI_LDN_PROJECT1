08e43435aa00bb1d42ea97deeda70ed6967c01c4
Jess Lodge <jessicaklodge@gmail.com>
2018-02-12 12:33:30 +0000

    Added spinner functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,92 @@
 function init() {
 
+  let player = null;
+  let Xturn = true;
+  const $display = $('#display');
+  const $board = $('#board');
+  const $screenbox = $('.screen');
+  const $button = $('button');
+  const $spinner = $('.circle-container');
+  const $circle = $('.circle');
+  const $timer = $('.timer');
+  const $nomore = $('.message');
+  const $getInitials = $('form');
+  const $welcome = $('.answer');
+  let choice = '';
+  let timer = 5;
+  let name = '';
 
-// need to start with the first list on active with maybe a 500ms delay
+  $nomore.hide();
+  // countdown timer
+  function countdown() {
+    const timerId = setInterval(() => {
+      if (timer === -1) {
+        clearInterval(timerId);
+        console.log('check');
+        doSomething();
+      } else {
+        $timer
+          .toggleClass('pulse')
+          .html(timer);
+        timer--;
+      }
+    }, 1000);
+  }
 
-// then loop through maybe using next changing the active status
-
-// then loop back round maybe using set interval every 1 second like timer doing the function you set it like
-
-//
-//
-// const countdown = document.querySelector('.octagon');
-// const screen = document.getElementsByClassName('screen')[1];
-// let timer = 30;
-// let timerIsRunning = false;
-// let timerID = null;
-// function runTimer() {
-//   if(!timerIsRunning) {
-//     timerID = setInterval(function(){
-//       screen.innerHTML = --timer;
-//       if(timer === 0) {
-//         clearInterval(timerID);
-//         alarm.classList.toggle('ringing');
-//         // timer = 10; // or if button clicked
-//       }
-//
-//     },1000);
-//     timerIsRunning = true;
-//   } else {
-//     clearInterval(timerID);
-//     timerIsRunning = false;
-//   }
-// }
-
-const $countdown = $('.screen');
+  function doSomething() {
+    // alert('No more bets');
+    $nomore.show();
+    $button.prop('disabled', true);
+    nextOne();
+  }
 
 
-$countdown.on('click', (e)=> {
-  $countdown.toggleClass('pulse');
-  $circle.toggleClass('ringing');
+  //move the class around $spinner
+  // function nextOne() {
+  //   let timesSpun = 0;
+  //   const startSpinner = setInterval(() => {
+  //     // const $lis = $('.circle-container > li');
+  //     const $current = $('.topheader');
+  //     let $next = $current.next();
+  //     // if no next then we're at the end
+  //     if( !$next.length ) {
+  //       timesSpun++;
+  //       $next = $current.parent().find('li:first'); // get the first li element in the parent
+  //     }
+  //     $next.addClass('.highlight');
+  //     $current.removeClass('.highlight');
+  //     if(timesSpun === 5) clearInterval(startSpinner);
+  //   }, 1000);
+  // }
+  // move the class around the board
+  function nextOne() {
+    console.log('next');
+    let timesSpun = 0;
+    const $lis = $('.circle-container > li');
+    let currentActive = 0;
+    setInterval(function() {
+      currentActive = (currentActive + 1) % $lis.length;
+      $lis.removeClass('flash').eq(currentActive).addClass('flash');
+    }, 150);
+  }
 
-  e.target
-  console.log('hello');
-});
+  $getInitials.submit(function(e) {
+    e.preventDefault();
+    const $input = $('#initials');
+    // const $name = $($input).val();
+    name = $input.val();
+    $input.val('');
+    console.log(name);
+    countdown();
+  });
+
+  // which color is chosen
+  $button.on('click', (e) => {
+    if(!name) return false;
+    choice = $(e.target).val();
+    console.log(choice);
+    $welcome.html(`${name} you chose ${choice}`);
+  });
 }
-window.addEventListener('DOMContentLoaded', init);
+
+$(init);
